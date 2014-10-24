d99fe6b220bd3f45b246178fec6e0c3168c5d096
Sobin Sunny <sobisunny@gmail.com>
2014-10-24 18:59:18 +0530

    Last commit

--- View/js/jss.js
+++ View/js/jss.js
@@ -1,51 +1,43 @@
 $(document).ready(function() {
-
-  $('#uname_button').click(function (e) {
-     
-              var u_name=$("#username").val();
-     
-  if(u_name.length>0)
-  {
-
-        $( "#uname" ).hide();
-        if ("WebSocket" in window)
-        {
-           
-           var ws = new WebSocket("ws://localhost:8080/"+u_name);
-           ws.onopen = function()
-           {
-           };
-           ws.onmessage = function (evt) 
-           { 
-               
-                      var i= evt.data;   
-                      div = document.createElement('div'); 
-                      $(div).attr('id','talkbubble').html("<p>"+i+"</p>");              
-                      $("#content_area").append(div);
-           };
-        ws.onclose = function()
-           { 
-              // websocket is closed.
-              alert("Connection is closed..."); 
-            };
-        }
-
-      else
-      {
-         // The browser doesn't support WebSocket
-         alert("WebSocket NOT supported by your Browser!");
-      }
-
+$('#uname_button').click(function (e) {
+var u_name=$("#username").val();
+if(u_name.length>0)
+{
+$( "#uname" ).hide();
+if ("WebSocket" in window)
+{
+var ws = new WebSocket("ws://localhost:8080/"+u_name);
+ws.onopen = function()
+{
+};
+ws.onmessage = function (evt)
+{
+var i= evt.data;
+div = document.createElement('div');
+$(div).attr('id','talkbubble').html("<p>"+i+"</p>");
+$("#content_area").append(div);
+};
+ws.onclose = function()
+{
+// websocket is closed.
+alert("Connection is closed...");
+};
+}
+else
+{
+// The browser doesn't support WebSocket
+alert("WebSocket NOT supported by your Browser!");
+}
 $('#send_button').click(function() {
-  var text=f.fname.value;
-  document.getElementById("fnameid").value = "      ";
-  ws.send(text);
+var text=f.fname.value;
+document.getElementById("fnameid").value = " ";
+ws.send(text);
 });
-      }
-      else
-      {
-        return false;
-      }
-      e.preventDefault();
+}
+else
+{
+return false;
+}
+e.preventDefault();
 });
 });
--- server.rb
+++ server.rb
@@ -1,30 +1,34 @@
 require 'em-websocket'
 
-EM.run do
-   
-    @clients = []
-     EM::WebSocket.start(:host => '0.0.0.0', :port => '8080') do |ws|
 
+EM.run do
+   @clients=[]
+    @names=[]
    EM::WebSocket.start(:host => '0.0.0.0', :port => '8080') do |ws|
     ws.onopen do |handshake|
             @clients << ws
             @name=handshake.path
-            @name=@name.sub("/" ,"")
+            @names<<@name.sub("/" ,"")
             puts "open";
         end
     ws.onclose do
             ws.send "Closed."
-            puts "Closed."
-            @clients.delete ws
+            @clients.delete(ws)
         end
 
     ws.onmessage do |msg|
-
+                puts ws
+                a=@clients.index(ws)
+                puts a
+                @username=@names[a]
+                puts @username
                 @clients.each do |socket|
-                socket.send "#{@name}:"+msg
-              end
+                     socket.send "#{@username}:"+msg
+                
+                end
                
         end
    end
 end
+
 
